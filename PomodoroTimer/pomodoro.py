ecda5e36a064a994ab29a16488b9a4d4da0435d3
HariNivash10 <harinivash111@gmail.com>
2026-07-05 11:19:41 +0530

    Fixed the overprinting bug

--- PomodoroTimer/pomodoro.py
+++ PomodoroTimer/pomodoro.py
@@ -1,19 +1,20 @@
 import time
 print("Pomodoro Timer")
 enter=input("Press Enter to start the timer...")
-for i in range(4):
+for i in range(1,5):
     a=1500
     b=300
-    print("Pomodoro cycles Done:", i)
     print("Work for 25 minutes")
     while a>0:
-        a-=1
         mins, sec=divmod(a,60)
-        print("Time left:", mins, ":",sec, end="\r", flush=True)
+        print(f"Time left: {mins:02d}:{sec:02d}", end="\r", flush=True)
         time.sleep(1)
+        a-=1
     print("Take a break for 5 minutes")
     while b>0:
-        b-=1
         mins, sec=divmod(b,60)
-        print("Break left:", mins, ":",sec, end="\r", flush=True)
+        print(f"Break left:{mins:02d}:{sec:02d}", end="\r", flush=True)
         time.sleep(1)
+        b-=1
+    print("Pomodoro cycles Done:", i)
+print("Awesome, Task finished")
