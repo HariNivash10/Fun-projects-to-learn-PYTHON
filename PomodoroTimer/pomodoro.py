73e24e313e05ab2129adcbe6f2d41f5ae0ce92af
HariNivash10 <harinivash111@gmail.com>
2026-07-06 10:23:41 +0530

    A small change to pomodoro timer and file updated

--- PomodoroTimer/pomodoro.py
+++ PomodoroTimer/pomodoro.py
@@ -7,7 +7,7 @@ for i in range(1,5):
     print("Work for 25 minutes")
     while a>0:
         mins, sec=divmod(a,60)
-        print(f"Time left: {mins:02d}:{sec:02d}", end="\r", flush=True)
+        print(f"Time left:{mins:02d}:{sec:02d}", end="\r", flush=True)
         time.sleep(1)
         a-=1
     print("Take a break for 5 minutes")
